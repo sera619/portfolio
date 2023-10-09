9db1360f86d28892e56d458cadd7e53d7a618c2e
S3R43o3 <67480273+sera619@users.noreply.github.com>
2023-10-09 15:25:22 +0200

    fix: game intro

--- src/components/game/Cards.jsx
+++ src/components/game/Cards.jsx
@@ -19,7 +19,7 @@ export default function Cards() {
 
     const [{ run, steps }, setSteps] = useState({
         run: false,
-        steps: introSteps.gameSteps
+        steps: introSteps.game
     })
 
     useEffect(() => {
@@ -103,17 +103,41 @@ export default function Cards() {
     function handleHelpCLick() {
         setSteps({ run: true, steps });
     }
-    const gameIntroSeen = localStorage.getItem("gameIntroSeen");
     useEffect(() => {
+        const gameIntroSeen = localStorage.getItem("gameIntroSeen");
         if (!gameIntroSeen) {
             handleHelpCLick();
             localStorage.setItem("gameIntroSeen", "true");
+            setSteps({ run: true, steps });
         }
     }, [])
 
 
     return (
         <>
+
+            <div className='flex m-auto justify-center align-center w-full z-20'>
+                <div className="max-w-100 my-5 flex flex-col sm:flex-row rounded-md shadow-sm text-center">
+                    <button id="step-2" onClick={resetGame}
+                        className={"border-1 px-5 py-2 my-1 sm:my-0 sm:mx-1 rounded duration-150 hover:border-[#00eeff] hover:text-[#00eeff] active:bg-[#00eeff] active:text-white"}>
+                        Restart</button>
+                    <button id="step-3" onClick={() => resetHighscore()}
+                        className={"border-1 px-5 py-2 my-1 sm:my-0 sm:mx-1  rounded duration-150 hover:border-[#ff0000] hover:text-[#ff0000] active:bg-[#ff0000] active:text-white"}>Delete
+                    </button>
+                    <Link id="step-4" to={"/portfolio/"}
+                        className={"border-1 px-5 py-2 my-1 sm:my-0 sm:mx-1  rounded duration-150 hover:border-[#00eeff] hover:text-[#00eeff] active:bg-[#00eeff] active:text-white"}>Home</Link>
+                    <button id="step-5" onClick={ handleHelpCLick}
+                        className={"border-1 px-5 py-2 my-1 sm:my-0 sm:mx-1  rounded duration-150 hover:border-[#00eeff] hover:text-[#00eeff] active:bg-[#00eeff] active:text-white"}>?</button>
+                </div>
+            </div>
+            {/* <p className="text-[#00ff00] text-[23px] mb-0">{winMessage.toString()}</p> */}
+            <p id="step-1" className="text-[#00ff00] mb-1 pb-1">Moves: <span classList={"text-primary"}>{moves}</span>&nbsp;&#9122;&nbsp;Record: {highscore}</p>
+
+            <div id="step-0" className="card-container">
+                {items.map((item, index) => (
+                    <GameCard key={index} item={item} id={index} handleClick={handleClick} />
+                ))}
+            </div>
             <Joyride
                 steps={steps}
                 scrollToFirstStep
@@ -132,28 +156,6 @@ export default function Cards() {
                     }
                 }}
             />
-            <div className='flex m-auto justify-center align-center w-full z-20'>
-                <div className="max-w-100 my-5 flex flex-col sm:flex-row rounded-md shadow-sm text-center">
-                    <button id="step-2" onClick={resetGame}
-                        className={"border-1 px-5 py-2 my-1 sm:my-0 sm:mx-1 rounded duration-150 hover:border-[#00eeff] hover:text-[#00eeff] active:bg-[#00eeff] active:text-white"}>
-                        Restart</button>
-                    <button id="step-3" onClick={() => resetHighscore()}
-                        className={"border-1 px-5 py-2 my-1 sm:my-0 sm:mx-1  rounded duration-150 hover:border-[#ff0000] hover:text-[#ff0000] active:bg-[#ff0000] active:text-white"}>Delete
-                    </button>
-                    <Link id="step-4" to={"/portfolio/"}
-                        className={"border-1 px-5 py-2 my-1 sm:my-0 sm:mx-1  rounded duration-150 hover:border-[#00eeff] hover:text-[#00eeff] active:bg-[#00eeff] active:text-white"}>Home</Link>
-                    <button id="step-5" onClick={() => handleHelpCLick()}
-                        className={"border-1 px-5 py-2 my-1 sm:my-0 sm:mx-1  rounded duration-150 hover:border-[#00eeff] hover:text-[#00eeff] active:bg-[#00eeff] active:text-white"}>?</button>
-                </div>
-            </div>
-            {/* <p className="text-[#00ff00] text-[23px] mb-0">{winMessage.toString()}</p> */}
-            <p id="step-1" className="text-[#00ff00] mb-1 pb-1">Moves: <span classList={"text-primary"}>{moves}</span>&nbsp;&#9122;&nbsp;Record: {highscore}</p>
-
-            <div id="step-0" className="card-container">
-                {items.map((item, index) => (
-                    <GameCard key={index} item={item} id={index} handleClick={handleClick} />
-                ))}
-            </div>
         </>
     )
 }
