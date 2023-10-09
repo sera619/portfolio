4423a9b2e80bff20accba0591736804e61b55845
S3R43o3 <67480273+sera619@users.noreply.github.com>
2023-10-09 15:07:56 +0200

    add: game intro

--- src/components/game/Cards.jsx
+++ src/components/game/Cards.jsx
@@ -114,6 +114,24 @@ export default function Cards(){
 
     return(
         <>
+        <Joyride
+                steps={steps}
+                scrollToFirstStep
+                continuous={true} 
+                showProgress
+                showSkipButton
+                run={run}
+                styles={{
+                    options:{
+                        backgroundColor: '#3333335e',
+                        textColor: '#3bc7eb',
+                        primaryColor: '#3bc7eb',
+                        arrowColor: '#3333335e',
+                        overlayColor: 'rgba(0, 0, 0, 0.9)',
+                        beaconSize: 54,
+                    }
+                }}
+                />
         <div className='flex m-auto justify-center align-center w-full z-20'>
         <div className="max-w-100 my-5 flex flex-col sm:flex-row rounded-md shadow-sm text-center">
             <button id="step-2" onClick={resetGame} 
@@ -124,7 +142,7 @@ export default function Cards(){
             </button>
             <Link id="step-4" to={"/portfolio/"} 
             className={"border-1 px-5 py-2 my-1 sm:my-0 sm:mx-1  rounded duration-150 hover:border-[#00eeff] hover:text-[#00eeff] active:bg-[#00eeff] active:text-white"}>Home</Link>
-            <button id="step-5" onClick={handleHelpCLick} 
+            <button id="step-5" onClick={() => handleHelpCLick()} 
             className={"border-1 px-5 py-2 my-1 sm:my-0 sm:mx-1  rounded duration-150 hover:border-[#00eeff] hover:text-[#00eeff] active:bg-[#00eeff] active:text-white"}>?</button>
         </div>
         </div>
